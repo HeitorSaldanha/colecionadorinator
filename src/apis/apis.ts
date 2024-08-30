50ec958cfd1496b83d8f8c0be8af92a5be900c5c
Heitor Saldanha <heitorsaldanha@gmail.com>
2024-08-30 17:40:30 +0200

    delete all

--- src/apis/apis.ts
+++
@@ -1,8 +0,0 @@
-export function useJerseysApi() {
-  //TODO Add DB integration
-  return {
-    teams: [{ value: "cam", label: "Alético Mineiro" }],
-    seasons: [{ value: "2024", label: "2024" }],
-    players: [{ value: "hulk", label: "Hulk" }],
-  };
-}
